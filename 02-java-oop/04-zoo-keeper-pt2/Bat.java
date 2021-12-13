7dfcd022abc06f66fda8a7cf50d2858da57420c1
Charlie Schiller <charlieschiller@Charlies-MacBook-Pro.local>
2021-12-13 06:38:02 -0800

    Zoo Keeper 2

---
+++ 02-java-oop/04-zoo-keeper-pt2/Bat.java
@@ -0,0 +1,19 @@
+public class Bat extends Mammal{
+
+    public Bat(int energyLevel){
+        super(energyLevel);
+    
+    }
+    public void fly(){
+        System.out.println("Flip Flap");
+        energyLevel -= 50;
+    }
+    public void eatHuman(){
+        System.out.println("Rare Please");
+        energyLevel += 25;
+    }
+    public void attackTown(){
+        System.out.println("Oh the Horror!");
+        energyLevel -=100;
+    }
+}
