cb1f57a794ad0240a5f534e867e41a371c1620b6
Charlie Schiller <charlieschiller@Charlies-MacBook-Pro.local>
2021-12-20 04:33:50 -0800

    Hello Human

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/helloHuman/src/main/java/com/example/helloHuman/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.helloHuman.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String hello(@RequestParam(value = "name", required = false, defaultValue = "Human") String name, Model model) {
+		model.addAttribute("name", name);
+		return "index.jsp";
+		
+	}
+
+}
